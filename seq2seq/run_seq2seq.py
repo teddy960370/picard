e06e85f5043035236bb0b6c1f337c5c625ad3df5
Ted Lee <dirty4538@gmail.com>
2024-02-13 21:20:26 +0800

    add new datacollator for causal LM

--- seq2seq/run_seq2seq.py
+++ seq2seq/run_seq2seq.py
@@ -18,7 +18,7 @@ from dataclasses import asdict, fields
 from transformers.hf_argparser import HfArgumentParser
 from transformers.training_args_seq2seq import Seq2SeqTrainingArguments
 from transformers.models.auto import AutoConfig, AutoTokenizer, AutoModelForSeq2SeqLM,AutoModelForCausalLM
-from transformers.data.data_collator import DataCollatorForSeq2Seq
+from transformers.data.data_collator import DataCollatorForSeq2Seq,DataCollatorForLanguageModeling
 from transformers.trainer_utils import get_last_checkpoint, set_seed
 from transformers.models.t5.modeling_t5 import T5ForConditionalGeneration
 from transformers.models.t5.tokenization_t5_fast import T5TokenizerFast
@@ -33,8 +33,9 @@ from seq2seq.utils.cosql import CoSQLTrainer
 from huggingface_hub import login
 
 import torch
-from transformers import BitsAndBytesConfig
+from transformers import BitsAndBytesConfig,Seq2SeqTrainer
 from peft import get_peft_config, PeftModel, PeftConfig, get_peft_model, LoraConfig, TaskType,prepare_model_for_kbit_training
+from DataCollatorForCausalLM import DataCollatorForCausalLM
 
 def main() -> None:
     # See all possible arguments by passing the --help flag to this script.
@@ -240,12 +241,19 @@ def main() -> None:
             "eval_dataset": dataset_splits.eval_split.dataset if training_args.do_eval else None,
             "eval_examples": dataset_splits.eval_split.examples if training_args.do_eval else None,
             "tokenizer": tokenizer,
-            "data_collator": DataCollatorForSeq2Seq(
-                tokenizer,
-                model=model,
-                #label_pad_token_id=(-100 if data_training_args.ignore_pad_token_for_loss else tokenizer.pad_token_id),
-                pad_to_multiple_of=8 if training_args.fp16 else None,
+            "data_collator" : DataCollatorForCausalLM(
+                tokenizer=tokenizer,
+                source_max_len=512,
+                target_max_len=512,
+                train_on_source=False,
+                predict_with_generate=False,
             ),
+            #"data_collator": DataCollatorForLanguageModeling(
+            #    tokenizer,
+            #    model=model,
+            #    label_pad_token_id=(-100 if data_training_args.ignore_pad_token_for_loss else tokenizer.pad_token_id),
+            #    pad_to_multiple_of=8 if training_args.fp16 else None,
+            #),
             "ignore_pad_token_for_loss": data_training_args.ignore_pad_token_for_loss,
             "target_with_db_id": data_training_args.target_with_db_id,
         }
